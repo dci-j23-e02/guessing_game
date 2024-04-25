72ff7f3e97b43dc8280270cacf9fd3860a60e710
safwan-kher <safwan.kherallah@digitalcareerinstitute.org>
2024-04-25 11:59:12 +0200

    commit

--- src/main/java/com/example/numbersguessinggame/GameController.java
+++ src/main/java/com/example/numbersguessinggame/GameController.java
@@ -1,12 +1,38 @@
 package com.example.numbersguessinggame;
 
+import java.util.Random;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 @Controller
+@SessionAttributes("number")
 public class GameController {
+
+  private Random random = new Random();
+
+// Initialize or reset the number
+  private void initializeNumber(Model model) {
+    int number = random.nextInt(100)+1;
+    model.addAttribute("number", number);
+    System.out.println("New number: " + number); // for developer to cheat ;)
+  }
+
+
 @GetMapping("/")
-  public String index(){
+  public String index(Model model){
+    if(!model.containsAttribute("number")){
+      initializeNumber(model);
+    }
   return "index";
 }
+
+
+  @GetMapping("/guess")
+  public  String guess(@RequestParam int guess,Model model){
+    // We will add our logic here
+  }
+
 }
