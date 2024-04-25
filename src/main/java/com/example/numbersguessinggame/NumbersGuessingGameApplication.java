61dfc7399a46854505c577c2c02ffaef4244746a
safwan-kher <safwan.kherallah@digitalcareerinstitute.org>
2024-04-25 11:09:25 +0200

    commit

--- src/main/java/com/example/numbersguessinggame/NumbersGuessingGameApplication.java
+++ src/main/java/com/example/numbersguessinggame/NumbersGuessingGameApplication.java
@@ -1,5 +1,13 @@
 package com.example.numbersguessinggame;
 
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class NumbersGuessingGameApplication {
 
+  public static void main(String[] args) {
+    SpringApplication.run(NumbersGuessingGameApplication.class, args);
+  }
 }
